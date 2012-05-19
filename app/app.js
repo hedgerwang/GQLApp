5d4cef5d855072ed97ab7ceb9f554259a29f7f52
Hedger Wang <hedgerwang+github@gmail.com>
2012-05-19 16:56:52 -0700

    New pattern for creating Class. Add Jewel.

--- app/app.js
+++ app/app.js
@@ -5,24 +5,31 @@
 
 var Chrome = require('jog/ui/chrome').Chrome;
 var Class = require('jog/class').Class;
-var EventType = require('app/eventtype').EventType;
 var Cover = require('app/ui/scene/cover').Cover;
+var EventType = require('app/eventtype').EventType;
+var NewsFeed = require('app/ui/scene/newsfeed').NewsFeed;
 var dom = require('jog/dom').dom;
 
-var App = Class.create({
+var App = Class.create(null, {
   main: function() {
     var chrome = new Chrome();
-
     var coverScene = new Cover();
+    var newsFeed = new NewsFeed();
+
+    // Show the initial blue screen.
     coverScene.render(chrome.getNode());
 
     coverScene.addEventListener(EventType.EVT_FB_SESSION_READY, function() {
-      alert(3);
+      newsFeed.render(chrome.getNode());
+      coverScene.dispose();
+      coverScene = null;
     });
 
+    // show the whole chrome.
     chrome.render(dom.getDocument().body);
   }
 });
+
 
 window.addEventListener('load', function() {
   new App();
--- app/ui/scene/cover/cover.js
+++ app/ui/scene/cover/cover.js
@@ -11,32 +11,28 @@ var cssx = require('jog/cssx').cssx;
 var dom = require('jog/dom').dom;
 var lang = require('jog/lang').lang;
 
-var Cover = Class.create({
-  main: function() {
-    Scene.call(this);
+var Cover = Class.create(Scene, {
+
+  /** @override} */
+  createNode: function() {
+    var node = Scene.prototype.createNode.call(this);
+    dom.addClassName(node, cssx('app-ui-scene-cover'));
+    return node;
   },
 
-  extend: Scene,
-
-  members: {
-    createNode: function() {
-      var node = this.superPrototype.createNode.call(this);
-      dom.addClassName(node, cssx('app-ui-scene-cover'));
-      return node;
-    },
-
-    onDocumentReady:function() {
-      FBAPI.isLoggedIn().addCallback(lang.bind(this, function(result) {
-        if (!this.isDisposed()) {
-          this.dispatchEvent(EventType.EVT_FB_SESSION_READY);
+  /** @override} */
+  onDocumentReady:function() {
+    FBAPI.isLoggedIn().addCallback(lang.bind(this, function(result) {
+      if (!this.isDisposed()) {
+        this.dispatchEvent(EventType.EVT_FB_SESSION_READY);
 //          dom.addClassName(this.getNode(), cssx('app-ui-scene-cover_ready'));
 //          FBAPI.query('me()').addCallback(lang.bind(this, function(response) {
 //            this.getNode().textContent = JSON.stringify(response);
 //          }));
-        }
-      }));
-    }
+      }
+    }));
   }
+
 });
 
 exports.Cover = Cover;
