a9b5c2293eb45489276b76aa0e47f8af12e830dd
Hedger Wang <hedgerwang+github@gmail.com>
2012-06-05 10:31:25 -0700

    crush private members.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 clear;
-python pylib/build_app.py
+python pylib/build_app.py > pylib/.js_compress_build/logs.txt
 cp index.build.html google_app_engine_host/build/index.html
 cp images/*.jpg google_app_engine_host/build/images
 cp images/*.gif google_app_engine_host/build/images
@@ -9,4 +9,5 @@ ls -al  google_app_engine_host/build/images
 ls -al pylib/.js_compress_build
 ls -al pylib/.css_compress_build
 
+echo pylib/.js_compress_build/logs.txt
 echo 'http://gqlapp.appspot.com:8888/index.build.html'
